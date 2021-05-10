d15f0bba1cc30de86123d06167ecd04c3207704a
YS Anirudh <ysanirudh@gmail.com>
2021-05-11 03:05:19 +0530

    Working on YPlane. Almost Done.

--- Math Engine/Libraries/MathLib/Plane.cpp
+++ Math Engine/Libraries/MathLib/Plane.cpp
@@ -3,28 +3,28 @@
 #include "GenMath.h"
 #include "Quaternion.h"
 #include "Matrix3x3.h"
+#include "Vector4D.h"
+#include "Matrix4x4.h"
 namespace MathLib {
     YPlane::YPlane() :
         normal(YVec3(1.0f, 0.0f, 0.0f)),
         offset(0.0f)
     {}
 
-    YPlane::YPlane(float a, float b, float c, float d) {
-        float lensq = a * a + b * b + c * c;
-        // length of normal had better not be zero
-        assert(!IsZero(lensq));
+    YPlane::YPlane(float InX, float InY, float InZ, float InW) {
+        float lensq = InX * InX + InY * InY + InZ * InZ;
+        
+        assert(!YMath::IsZero(lensq));
 
-        // recover gracefully
-        if (IsZero(lensq))
+        if (YMath::IsZero(lensq))
         {
             normal = YVec3(1.0f, 0.0f, 0.0f);// IvVector3::xAxis;
             offset = 0.0f;
         }
         else
         {
-            float recip = 1/Sqrt(lensq);
             normal = YVec3(1.0f, 0.0f, 0.0f);
-            offset = d * recip;
+            offset = InW / YMath::Sqrt(lensq);
         }
     }
 
@@ -40,6 +40,27 @@ namespace MathLib {
     {}
     
     YPlane::YPlane(const YVec3& Normal, float Offset) :normal(Normal), offset(Offset) {}
+    
+    YPlane::YPlane(const YVec3& InPoint, const YVec3& InNormal) {
+        
+    }
+    
+    YPlane::YPlane(const YVec4& V) {
+        float lensq = V.x * V.x + V.y * V.y + V.z * V.z;
+        
+        assert(!YMath::IsZero(lensq));
+
+        if (YMath::IsZero(lensq))
+        {
+            normal = YVec3(1.0f, 0.0f, 0.0f);// IvVector3::xAxis;
+            offset = 0.0f;
+        }
+        else
+        {
+            normal = YVec3(1.0f, 0.0f, 0.0f);
+            offset = V.w / YMath::Sqrt(lensq);
+        }
+    }
 
     bool YPlane::operator==(const YPlane& Plane) const {
         return (Plane.normal == normal && YMath::AreEqual(Plane.offset, offset));
@@ -48,8 +69,103 @@ namespace MathLib {
     bool YPlane::operator!=(const YPlane& Plane) const {
         return !(Plane.normal == normal && YMath::AreEqual(Plane.offset, offset));
     }
+    
+    inline YPlane YPlane::operator+(const YPlane& Plane) const {
+        return YPlane(normal + Plane.normal, offset + Plane.offset);
+    }
 
-    YPlane YPlane::Transform(float scale, const YQuat& rotate, const YVec3& translate) const {
+    inline YPlane YPlane::operator+=(const YPlane& Plane)  {
+        return (*this) + Plane;
+    }
+    
+    inline YPlane YPlane::operator-(const YPlane& Plane) const {
+        return YPlane(normal - Plane.normal, offset - Plane.offset);
+    }
+    
+    inline YPlane YPlane::operator-=(const YPlane& Plane) {
+        return (*this) - Plane;
+    }
+    
+    inline YPlane YPlane::operator*(const YPlane& Plane) const {
+        
+    }
+    
+    inline YPlane YPlane::operator*=(const YPlane& Plane) const {
+        
+    }
+    
+    inline YPlane YPlane::operator*(float Scale) const {
+       return YPlane(normal * Scale, offset * Scale);
+    }
+    
+    inline YPlane operator*(float Scale, const YPlane& Plane) {
+        return Plane * Scale;
+    }
+    
+    inline YPlane YPlane::operator*=(float Scale) {
+        return (*this) * Scale;
+        
+    }
+    
+    inline YPlane YPlane::operator/(float Scale) const {
+        Scale = 1/ Scale;
+        return YPlane(normal * Scale, offset * Scale);
+    }
+    
+    inline YPlane YPlane::operator/=(float Scale) {
+        return (*this) / Scale;
+    }
+    
+    inline float YPlane::operator|(const YPlane& Plane) const {
+        
+    }
+
+    inline YVec3 YPlane::ClosestPoint(const YVec3& point) const {
+        return point - (normal.dot(point) + offset) * normal;
+    }
+    
+    inline bool YPlane::Equals(const YPlane& Plane, float Tolerance) const {
+        if (normal.Equals(Plane.normal, Tolerance) && 
+            YMath::AreNearlyEqual(offset, Plane.offset, Tolerance)
+        ) {
+            return true;
+        }
+        return false;
+    }
+    
+    inline YPlane YPlane::Flip() const {
+        return YPlane(-normal, offset);
+    }
+    
+    inline YVec3 YPlane::GetNormal() const {
+        return normal;
+    }
+    
+    inline YVec3 YPlane::GetOrigin() const {
+        YVec3 temp = normal.GetSafeNormal();
+        return temp * offset;
+    }
+    
+    inline bool YPlane::IsValid() const {
+        if (!YMath::IsZero(normal.MagnitudeSquared())) {
+            return false;
+        }
+        return true;
+    }
+    
+    inline float YPlane::PlaneDot(const YVec3& V) const {
+        
+    }
+    
+    inline YPlane YPlane::TransformBy(const YMat4x4& M) const {
+        
+    }
+    
+    inline YPlane YPlane::TransformBy(const YQuat& Quat) const {
+        
+    }
+
+    inline YPlane YPlane::TransformBy(float scale, const YQuat& rotate, const YVec3& translate) const {
         YPlane plane;
 
         // get rotation matrix
@@ -65,7 +181,9 @@ namespace MathLib {
         return plane;
     }
 
-    YVec3 YPlane::ClosestPoint(const YVec3& point) const {
-        return point - Test(point) * normal;
+    
+    inline bool YPlane::Normalize(float Tolerance) 
+    {
+        
     }
 }
--- Math Engine/Libraries/MathLib/Plane.h
+++ Math Engine/Libraries/MathLib/Plane.h
@@ -5,36 +5,56 @@ namespace MathLib {
     class YVec3;
     class YQuat;
     class YMat3x3;
+    class YMat4x4;
+    class YVec4;
 	class YPlane {
     public:
-        YVec3 normal;
         float offset;
+        YVec3 normal;
         YPlane();
         YPlane(const YVec3& Normal, float Offset);
-        YPlane(float a, float b, float c, float d);
-        //YPlane(const YVec3& p0, const YVec3& p1, const YVec3& p2);
+        YPlane(const YVec3& InPoint, const YVec3& InNormal);
+        YPlane(float InX, float InY, float InZ, float InW);
+        YPlane(const YVec4& V);
+        
+        //YPlane(YVec3 A, YVec3 C, YVec3 C);
 
         // copy operations
         YPlane(const YPlane& other);
 
-        bool operator==(const YPlane& Plane) const;
-        bool operator!=(const YPlane& Plane) const;
+        inline bool operator==(const YPlane& Plane) const;
+        inline bool operator!=(const YPlane& Plane) const;
 
-        YPlane Transform(float scale, const YQuat& rotation,
-            const YVec3& translation) const;
+        inline YPlane operator+(const YPlane& Plane) const;
+        inline YPlane operator+=(const YPlane& Plane);
+        inline YPlane operator-(const YPlane& Plane) const;
+        inline YPlane operator-=(const YPlane& Plane);
+        inline YPlane operator*(const YPlane& Plane) const;
+        inline YPlane operator*=(const YPlane& Plane) const;
+        inline YPlane operator*(float Scale) const;
+        inline friend YPlane operator*(float Scale, const YPlane& Plane);
+        inline YPlane operator*=(float Scale);
+        inline YPlane operator/(float Scale) const;
+        inline YPlane operator/=(float Scale);
+        inline float operator|(const YPlane& Plane) const;
+        
 
-        // distance
-        inline friend float Distance(const YPlane& plane, const YVec3& point) {
-            return YMath::Abs(plane.Test(point));
-        }
+        inline bool Equals(const YPlane& Plane, float Tolerance) const;
+        inline YPlane Flip() const;
+        inline YVec3 GetNormal() const;
+        inline YVec3 GetOrigin() const;
+        inline bool IsValid() const;
+        inline float PlaneDot(const YVec3& V) const;
+        inline YPlane TransformBy(const YMat4x4& M) const;
+        inline YPlane TransformBy(const YQuat& Quat) const;
+        inline YPlane TransformBy(
+            float scale, 
+            const YQuat& rotation,
+            const YVec3& translation
+        ) const;
 
+        inline bool Normalize(float Tolerance);
         // closest point
-        YVec3 ClosestPoint(const YVec3& point) const;
-
-        // result of plane test
-        inline float Test(const YVec3& point) const
-        {
-            return normal.dot(point) + offset;
-        }
+        inline YVec3 ClosestPoint(const YVec3& point) const;
 	};
 }
